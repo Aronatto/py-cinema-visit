380db05efbc0c931e4f59e55714f10ffd0feac36
Aronatto <koksik122@o2.pl>
2025-12-17 09:05:30 +0100

    Revert "Develop"

--- app/main.py
+++ app/main.py
@@ -1,22 +1,6 @@
-from app.cinema.bar import CinemaBar
-from app.cinema.hall import CinemaHall
-from app.people.customer import Customer
-from app.people.cinema_staff import Cleaner
+# write your imports here
 
 
-def cinema_visit(customers, hall_number, cleaner,  movie):
-    customer_objects = []
-
-    for data in customers:
-        customer_obj = Customer(name=data["name"], food=data["food"])
-        customer_objects.append(customer_obj)
-        CinemaBar.sell_product(product=data["food"], customer=customer_obj)
-
-    hall = CinemaHall(number=hall_number)
-    cleaner_obj = Cleaner(name=cleaner)
-
-    hall.movie_session(
-        movie_name=movie,
-        customers=customer_objects,
-        cleaning_staff=cleaner_obj
-    )
+def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
+    # write you code here
+    pass
--- app/people/cinema_staff.py
+++
@@ -1,6 +0,0 @@
-class Cleaner:
-    def __init__(self, name):
-        self.name = name
-
-    def clean_hall(self, hall_number):
-        print(f"Cleaner {self.name} is cleaning hall number {hall_number}.")
